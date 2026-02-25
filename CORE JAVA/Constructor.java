ce6e16d6131a48d50405eb5a17f10207ff649c7b
Md Ambar Shafi <shafiambar3@gmail.com>
2026-02-25 23:16:53 +0530

    Encapsulation

--- CORE JAVA/Constructor.java
+++ CORE JAVA/Constructor.java
@@ -3,6 +3,20 @@ class Human{
     private int age;
     private String name;
 
+
+    // Defoult Constructor
+    public Human(){
+        age = 12;
+        name = "Ambar";
+    }
+
+    // Parametorized Constructor
+    public Human(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+
     // getter
     public int getAge(){
         return age;
@@ -24,16 +38,19 @@ class Human{
 }
 
 
-public class Encapsulation{
+public class Constructor{
 
     public static void main(String args[]){
 
         Human obj = new Human();
+
+        Human obj1 = new Human(20,"Shahid");
         // setter
         obj.setAge(21);
         obj.setName("Shafi");
 
         // getter
         System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
     }
 }
